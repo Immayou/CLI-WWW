ef34caccc227d8f9c5c7320601b24f3fbe88e4d0
Yuliya Zozulya <djulia@ukr.net>
2023-05-22 12:00:51 +0300

    fixing bugs

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const questions = [
     type: "input",
     message: "Введіть 10 слів або чисел, розділяючи їх пробілом",
     validate: (input) => {
+      if (input === "exit") {
+        console.log("\n" + "Goodbye!");
+        process.exit();
+      }
       const arrayOfValues = input
         .trim()
         .split(" ")
@@ -18,17 +22,33 @@ const questions = [
   },
   {
     name: "operation",
-    type: "rawlist",
+    type: "input",
     message:
-      "Яку з наведених нижче операцій ви б хотіли виконати зі словами та числами? Оберіть варіант та натисніть Enter.",
-    choices: [
-      "Розсортуйте слова за алфавітом",
-      "Показувати числа від меншого до більшого",
-      "Показуйте числа від більших до менших",
-      "Відображення слів у порядку зростання за кількістю букв у слові",
-      "Показувати лише унікальні слова",
-      "Відображати лише унікальні значення з набору слів і чисел, введених користувачем",
-    ],
+      "Яку з наведених нижче операцій ви б хотіли виконати зі словами та числами? Введіть цифру варіанту та натисніть Enter." +
+      "\n" +
+      "1. Розсортуйте слова за алфавітом" +
+      "\n" +
+      "2. Показувати числа від меншого до більшого" +
+      "\n" +
+      "3. Показуйте числа від більших до менших" +
+      "\n" +
+      "4. Відображення слів у порядку зростання за кількістю букв у слові" +
+      "\n" +
+      "5. Показувати лише унікальні слова" +
+      "\n" +
+      "6. Відображати лише унікальні значення з набору слів і чисел, введених користувачем" +
+      "\n" +
+      "Option: ",
+    validate: (input) => {
+      if (input.toLowerCase() === "exit") {
+        console.log("\n" + "Goodbye!");
+        process.exit();
+      }
+      if (!Number(input) || Number(input) < 1 || Number(input) > 6) {
+        return "Введіть цифру 1-6 відповідно до обраної операції";
+      }
+      return true;
+    },
   },
 ];
 
@@ -37,147 +57,160 @@ function workWithValues() {
   const arrayOfValues = values
     .trim()
     .split(" ")
+    .map((item) => item.toLowerCase())
     .filter((item) => item !== "");
-  const words = arrayOfValues.filter((item) => !Number(item));
+  const words = arrayOfValues
+    .map((item) => item.toLowerCase())
+    .filter((item) => !Number(item));
   const numbers = arrayOfValues.filter((item) => Number(item));
   switch (operation) {
-    case "Розсортуйте слова за алфавітом":
+    case "1":
       const sortedWords = [...words].sort();
       console.log(sortedWords);
+      prompt(workWithValues);
       break;
-    case "Показувати числа від меншого до більшого":
+    case "2":
       const sortedNumbersInc = [...numbers].sort();
       console.log(sortedNumbersInc);
+      prompt(workWithValues);
       break;
-    case "Показуйте числа від більших до менших":
+    case "3":
       const sortedNumbersDec = [...numbers].sort((a, b) => b - a);
       console.log(sortedNumbersDec);
       break;
-    case "Відображення слів у порядку зростання за кількістю букв у слові":
+    case "4":
       const sortedWordsByLength = [...words].sort((a, b) => {
         a.length - b.length;
       });
       console.log(sortedWordsByLength);
+      prompt(workWithValues);
       break;
-    case "Показувати лише унікальні слова":
+    case "5":
       const uniqueWords = words.filter(
         (item, index, array) => array.indexOf(item) === index
       );
       console.log(uniqueWords);
+      prompt(workWithValues);
       break;
-    case "Відображати лише унікальні значення з набору слів і чисел, введених користувачем":
+    case "6":
       const uniqueValues = arrayOfValues.filter(
         (item, index, array) => array.indexOf(item) === index
       );
       console.log(uniqueValues);
+      prompt(workWithValues);
       break;
     default:
       console.log(values);
   }
 }
 
-inquirer.prompt(questions).then(workWithValues);
+const prompt = (callback) => {
+  inquirer.prompt(questions).then(callback);
+};
 
-// // Exit the inquirer prompt
-// function exit() {
-//   prompt.ui.close();
-// }
+prompt(workWithValues);
 
-// // close inquirer input if user press "escape" key
-// process.stdin.on("keypress", (_, key) => {
-//   if (key.name === "escape") {
-//     exit();
-//   }
-// });
-
-// // Declare your prompt and save it
-// const prompt = inquirer.prompt({
-//   name: "myInput",
-//   type: "input",
-//   // ...
-// });
-
-// // Listen when the user submit a value
-// prompt.then((values) => {
-//   console.log(values["myInput"]);
-//   // ...
-// });
 // import inquirer from "inquirer";
 
-// function getGithubCredentials(callback) {
-//   const questions = [
-//     {
-//       name: "values",
-//       type: "input",
-//       message: "Введіть 10 слів або чисел, розділяючи їх пробілом",
-//       validate: (input) => {
-//         const arrayOfValues = input.split(" ");
-//         const arrayWithoutSpaces = arrayOfValues.filter((item) => item !== "");
-//         if (arrayWithoutSpaces.length !== 10) {
-//           return `Введена вами кількість слів або чисел - ${arrayWithoutSpaces.length}. Будь ласка, введіть 10 слів або чисел.`;
-//         }
-//         return true;
-//       },
+// const questions = [
+//   {
+//     name: "values",
+//     type: "input",
+//     message: "Введіть 10 слів або чисел, розділяючи їх пробілом",
+//     validate: (input) => {
+//       if (input === "exit") {
+//         console.log("\n" + "Goodbye!");
+//         process.exit();
+//       }
+//       const arrayOfValues = input
+//         .trim()
+//         .split(" ")
+//         .filter((item) => item !== "");
+//       if (arrayOfValues.length !== 10) {
+//         return `Введена вами кількість слів або чисел - ${arrayOfValues.length}. Будь ласка, введіть 10 слів або чисел.`;
+//       }
+//       return true;
 //     },
-//     {
-//       name: "operation",
-//       type: "rawlist",
-//       message:
-//         "Яку з наведених нижче операцій ви б хотіли виконати зі словами та числами? Оберіть варіант та натисніть Enter.",
-//       choices: [
-//         "Розсортуйте слова за алфавітом",
-//         "Показувати числа від меншого до більшого",
-//         "Показуйте числа від більших до менших",
-//         "Відображення слів у порядку зростання за кількістю букв у слові",
-//         "Показувати лише унікальні слова",
-//         "Відображати лише унікальні значення з набору слів і чисел, введених користувачем",
-//       ],
-//       validate: (options) => {
-//         if (options) {
-//           console.log(options);
-//           return true;
-//         }
-//         return true;
-//       },
+//   },
+//   {
+//     name: "operation",
+//     type: "rawlist",
+//     message:
+//       "Яку з наведених нижче операцій ви б хотіли виконати зі словами та числами? Оберіть варіант або введіть цифру варіанту та натисніть Enter.",
+//     choices: [
+//       "Розсортуйте слова за алфавітом",
+//       "Показувати числа від меншого до більшого",
+//       "Показуйте числа від більших до менших",
+//       "Відображення слів у порядку зростання за кількістю букв у слові",
+//       "Показувати лише унікальні слова",
+//       "Відображати лише унікальні значення з набору слів і чисел, введених користувачем",
+//     ],
+//     filter: function (val) {
+//       console.log(val[0]);
 //     },
-//   ];
+//     //   if (input === "exit") {
+//     //     console.log("Goodbye!");
+//     //     process.exit();
+//     //   }
+//     //   return input;
+//     // },
+//   },
+// ];
 
-//   inquirer.prompt(questions).then(callback);
-// }
-
-// getGithubCredentials(function () {
+// function workWithValues() {
 //   const { values, operation } = arguments[0];
-//   const arrayOfValues = values.split(" ");
-//   const words = arrayOfValues.filter((item) => !Number(item));
+//   const arrayOfValues = values
+//     .trim()
+//     .split(" ")
+//     .map((item) => item.toLowerCase())
+//     .filter((item) => item !== "");
+//   const words = arrayOfValues
+//     .map((item) => item.toLowerCase())
+//     .filter((item) => !Number(item));
 //   const numbers = arrayOfValues.filter((item) => Number(item));
 //   switch (operation) {
 //     case "Розсортуйте слова за алфавітом":
 //       const sortedWords = [...words].sort();
 //       console.log(sortedWords);
+//       prompt(workWithValues);
 //       break;
 //     case "Показувати числа від меншого до більшого":
-//       const sortedNumbers = [...numbers].sort();
-//       console.log(sortedNumbers);
+//       const sortedNumbersInc = [...numbers].sort();
+//       console.log(sortedNumbersInc);
+//       prompt(workWithValues);
+//       break;
+//     case "Показуйте числа від більших до менших":
+//       const sortedNumbersDec = [...numbers].sort((a, b) => b - a);
+//       console.log(sortedNumbersDec);
 //       break;
 //     case "Відображення слів у порядку зростання за кількістю букв у слові":
 //       const sortedWordsByLength = [...words].sort((a, b) => {
 //         a.length - b.length;
 //       });
 //       console.log(sortedWordsByLength);
+//       prompt(workWithValues);
 //       break;
 //     case "Показувати лише унікальні слова":
 //       const uniqueWords = words.filter(
 //         (item, index, array) => array.indexOf(item) === index
 //       );
 //       console.log(uniqueWords);
+//       prompt(workWithValues);
 //       break;
 //     case "Відображати лише унікальні значення з набору слів і чисел, введених користувачем":
 //       const uniqueValues = arrayOfValues.filter(
 //         (item, index, array) => array.indexOf(item) === index
 //       );
 //       console.log(uniqueValues);
+//       prompt(workWithValues);
 //       break;
 //     default:
-//       break;
+//       console.log(values);
 //   }
-// });
+// }
+
+// const prompt = (callback) => {
+//   inquirer.prompt(questions).then(callback);
+// };
+
+// prompt(workWithValues);
